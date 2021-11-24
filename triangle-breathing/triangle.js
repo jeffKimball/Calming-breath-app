4b6d850086965647cb5e86c404389a9f1939c800
Jeff Kimball <jeffkimballis@gmail.com>
2021-11-23 21:35:08 -0600

    complete breathing function timing

---
+++ triangle-breathing/triangle.js
@@ -0,0 +1,38 @@
+const container = document.getElementById('container');
+const clickStart = document.getElementById('click-start');
+const titleStart = document.getElementById('title-start')
+const iconStart = document.getElementById('icon-start')
+const text = document.getElementById('text');
+const pointerContainer = document.querySelector('.pointer-container')
+
+const totalTime = 12000;
+const breatheTime = 4000;
+const holdTime = 4000;
+
+clickStart.addEventListener('click', ()=>{
+  clickStart.style.display = "none"
+  titleStart.style.display = "none"
+  iconStart.style.display = "none"
+  pointerContainer.style.animation = "rotate 12s linear forwards infinite"
+  breathAnimation()
+  setInterval(breathAnimation, totalTime);
+})
+
+
+// breathAnimation();
+
+function breathAnimation() {
+  
+  text.innerText = 'Breathe In';
+  container.className = 'container grow';
+
+   setTimeout(()=>{
+      text.innerText = 'Hold';
+
+      setTimeout(()=> {
+        text.innerText = 'Breathe Out';
+        container.className = 'container shrink';
+      }, 4000)
+   }, 4000) 
+}
+
